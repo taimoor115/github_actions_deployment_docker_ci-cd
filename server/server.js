0d3224330b216d98fb0cf3dbb6b228c66aaef01a
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-10-18 18:53:19 +0500

    fix: Update health check response message to indicate server is working

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.status(200).send("Server is running😄");
+  res.status(200).send("Server is working...😄");
 });
 app.use("/api/books", bookRoutes);
 
